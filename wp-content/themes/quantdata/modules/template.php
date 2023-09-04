0a57310c855085f0dfe13c753e6861536f369734
mamaclay-michaelangelo <michaelangelo.mamaclay@iphitech.com>
2023-09-04 12:20:36 +0800

    REvert changes

--- wp-content/themes/quantdata/modules/template.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  $heading = $fields['heading'];
-?>
-
-<div class="module module-template <?= !empty( $attributes['className'] ) ? $attributes['className'] : '' ?>">
-  <div class="wrapper">
-
-  </div>
-</div>
